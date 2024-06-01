91a2df696b39c14710effb45b88b83729ebacb10
Levan Esitashvili <124806271+Lvnii@users.noreply.github.com>
2024-06-02 01:42:28 +0400

    commit
    hero-component,
    triplet-component.

--- my-app/src/Pages/Home/Home.tsx
+++ my-app/src/Pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../Store/hooks";
 import fetchProducts from "../../Store/Products/Products.async.Actions";
-import earphones from '../../../public/assets/shared/desktop/image-category-thumbnail-earphones.png'
-import headphones from '../../../public/assets/shared/desktop/image-category-thumbnail-headphones.png'
-import speakers from '../../../public/assets/shared/desktop/image-category-thumbnail-speakers.png'
+import earphones from '../../../public/assets/shared/desktop/earphone-thumbnail.png'
+import headphones from '../../../public/assets/shared/desktop/headphone-thumbnail.png'
+import speakers from '../../../public/assets/shared/desktop/speaker-thumbnail.png'
+import rightArrow from '../../../public/assets/shared/desktop/icon-arrow-right.svg'
+import thumbShadow from '../../../public/assets/shared/desktop/thumbnail-shadow.png'
 import './Home.css'
 
 const Home = () => {
@@ -19,6 +21,7 @@ const Home = () => {
 
     return (
         <div className="home-page">
+            {/***************     hero    *****************/}
             <div className="hero-head">
                 <div className="left-side">
                     <h3 className="headline">new product</h3>
@@ -29,15 +32,46 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            {/***************     triplets    *****************/}
             <div className="triplets">
                 <div className="product">
-                    <img src={headphones} alt="headphones" />
+                    <div className="product-icon-div">
+                        <img className="thumbnail-icon" src={headphones} alt="headphones" />
+                        <img className="thumbnail-shadow" src={thumbShadow} alt="shadow" />
+                    </div>
+                    <div className="inside-product">
+                        <h4 className="headline">headphones</h4>
+                        <div className="button-div">
+                            <button className="triplet-button">shop</button>
+                            <img className="right-svg" src={rightArrow} alt="arrow" />
+                        </div>
+                    </div>
                 </div>
                 <div className="product">
-                    <img src={speakers} alt="headphones" />
+                    <div className="product-icon-div">
+                        <img className="thumbnail-icon" src={speakers} alt="speakers" />
+                        <img className="thumbnail-shadow"  src={thumbShadow} alt="shadow" />
+                    </div>
+                    <div className="inside-product">
+                        <h4 className="headline">speakers</h4>
+                        <div className="button-div">
+                            <button className="triplet-button">shop</button>
+                            <img className="right-svg" src={rightArrow} alt="arrow" />
+                        </div>
+                    </div>
                 </div>
                 <div className="product">
-                    <img src={earphones} alt="headphones" />
+                    <div className="product-icon-div">
+                        <img className="thumbnail-icon" src={earphones} alt="earphones" />
+                        <img className="thumbnail-shadow" src={thumbShadow} alt="shadow" />
+                    </div>
+                    <div className="inside-product">
+                        <h4 className="headline">earphones</h4>
+                        <div className="button-div">
+                            <button className="triplet-button">shop</button>
+                            <img className="right-svg" src={rightArrow} alt="arrow" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
